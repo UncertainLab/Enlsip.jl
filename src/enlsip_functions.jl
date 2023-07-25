8e0f964395b0c302f09a1d17a1a4079e4d917350
Pierre Borie <pierre.borie98@gmail.com>
2023-07-25 01:21:45 -0400

    correction of a overwritten function

--- src/enlsip_functions.jl
+++ src/enlsip_functions.jl
@@ -1,31 +1,3 @@
-
-# EVSCAL 
-# Scale jacobian matrix of active constraints A and active constraints evaluation vector cx if so indicated (ie if scale different from 0) by forming vectors :
-# diag*A and diag*cx
-# where diag is an array of dimension whose i-th element equals either ||∇c_i(x)|| or  (1/||∇c_i(x)|) depending on wether scaling is done or not. 
-# The vectors are formed by modifying in place matrix A and vector cx 
-
-function evaluate_scaling!(C::Constraint)
-
-    t = size(C.A, 1)
-    ε_float = eps(Float64)
-    C.diag_scale = zeros(t)
-    for i = 1:t
-        row_i = norm(C.A[i, :])
-        C.diag_scale[i] = row_i
-        if C.scaling
-            if abs(row_i) < ε_float
-                row_i = 1.0
-            end
-            C.A[i, :] /= row_i
-            C.cx[i] /= row_i
-            C.diag_scale[i] = 1.0 / row_i
-        end
-    end
-    return
-end
-
-
 #=
     pseudo_rank (diag_T, ε_rank)
 
