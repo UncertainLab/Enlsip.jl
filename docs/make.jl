5e5ba4930d160d146e50a2544f722d05bbca8592
Pierre Borie <66522252+pierre-borie@users.noreply.github.com>
2023-09-11 14:50:39 +0200

    fichier yml pour generer documentqation via github actions

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,11 @@ makedocs(
     ]
 )
 
+deploydocs(
+    repo = "github.com/UncertainLab/Enlsip.jl.git",
+    devbranch = "master"
+)
+
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
